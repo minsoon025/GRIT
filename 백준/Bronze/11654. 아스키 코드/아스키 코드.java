b220fc072cfda352b9a0f41df8050aa3bc54f64c
MINTAE KIM <112068306+minsoon025@users.noreply.github.com>
2023-02-17 10:01:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 12784 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char[] a = str.toCharArray();
+		int num = a[0];
+		System.out.println(num);
+				
+	}
+	
+
+}
