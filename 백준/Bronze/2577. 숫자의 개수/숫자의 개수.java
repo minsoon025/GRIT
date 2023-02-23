2014005a1b946af5d9c9fe73ddf49c877942a08b
MINTAE KIM <112068306+minsoon025@users.noreply.github.com>
2023-02-23 11:06:04 +0900

    [Bronze II] Title: 숫자의 개수, Time: 112 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int sum = (a*b)*c;
+		String str = Integer.toString(sum);
+		
+		
+		
+		for(int i=0; i<10; i++) {
+			int cnt = 0;
+			for(int j=0; j<str.length(); j++) {
+				if((str.charAt(j)-'0') == i) {
+					cnt++;
+				}
+				
+			}
+			System.out.println(cnt);
+			
+			
+			
+		}
+		
+		
+	}
+
+}
