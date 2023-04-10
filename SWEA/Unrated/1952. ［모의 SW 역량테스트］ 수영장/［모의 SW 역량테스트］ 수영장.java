ccb9a51728dfc0cb813c62ec6f8d11531a4c7492
MINTAE KIM <112068306+minsoon025@users.noreply.github.com>
2023-04-10 15:02:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 145 ms, Memory: 23,796 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc= new Scanner(System.in);
 		
 		int T = sc.nextInt();
-		for(int tc = 1; tc<=T; tc++) {
-			//1일 이용권
-			int one  =sc.nextInt();
-			//1달이용권
-			int month  =sc.nextInt();
-			//3달이용권
-			int three  =sc.nextInt();
-			//1년 이용권
-			int year  =sc.nextInt();
-			
-			// 1년간 계획
-			int[] plan = new int[13];
-			for(int i = 1; i<=12; i++) {
-				int day  = sc.nextInt();
-				plan[i] = Math.min(plan[i-1]+day*one, plan[i-1] + month);
-				// 3달 이상 넘어가면 
-				if(i>=3) plan[i] = Math.min(plan[i], plan[i-3]+ three);
-				
-			}
-			int min = Math.min(plan[12], year);
-			System.out.println("#" + tc + " " + min);
-			
-			
+		for(int tc =1; tc<=T; tc++) {
+		
+		// 1일, 1달, 3달, 12개월
+		int day = sc.nextInt();
+		int month = sc.nextInt();
+		int three = sc.nextInt();
+		int year =  sc.nextInt();
+		
+		int[] arr = new int[13];
+		for(int i = 1; i<=12; i++) {
+			int per = sc.nextInt();
+			// 3달 전까지를 카운트 해준다.
+			arr[i] = Math.min(arr[i-1]+ per*day, arr[i-1]+ month);
+			// 하루하루 계산하는 것과 3개월 치를 비교하는 것을 비교해 준다.
+			if(i>=3) arr[i] = Math.min(arr[i], arr[i-3] + three);
 		}
+		// 1년 치랑 하나하나 계산한 값을 비교
+		int min = Math.min(arr[12], year);
+		System.out.println("#"+ tc + " " + min);
+		
+		
+		}
+		
+		
 	}
 
 }
