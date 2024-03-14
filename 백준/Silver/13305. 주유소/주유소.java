15760e1ad116350912a843f98cc04124dffe4b0a
MINTAE KIM <112068306+minsoon025@users.noreply.github.com>
2024-03-14 17:14:53 +0900

    [Silver III] Title: 주유소, Time: 988 ms, Memory: 168412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,37 +1,43 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        long[] dist = new long[N-1];
-        long[] price = new long[N]; 
-
-            
-        // 거리 입력 
+import java.util.Scanner;
+ 
+public class Main {
+	
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		long[] dist = new long[N - 1];	// 거리
+		long[] cost = new long[N];	// 비용 
+		
+		// 거리 입력 
 		for(int i = 0; i < N - 1; i++) {
-			dist[i] = sc.nextLong();
+			dist[i] = in.nextLong();
 		}
 		
 		// 리터당 기름값 입력
 		for(int i = 0; i < N; i++) {
-			price[i] = sc.nextLong();
+			cost[i] = in.nextLong();
 		}
-        // 가격 더하가
-        long total = 0;
-        // 최저값이 나오면, 업데이트
-        long mini = 9999999;
-
-        for(int i=0; i<N-1; i++){
-            if(price[i] < mini) 
-                mini = price[i];
-                 total += (mini*dist[i]);
-            }
-            System.out.println(total);
-        
-
-
-    }
+		
+		long sum = 0;
+		long minCost = cost[0];	// 이전 까지의 과정 중 주유 최소 비용 
+ 
+		for(int i = 0; i < N - 1; i++) {
+			
+			/*
+			 *  현재 주유소가 이전 주유소의 기름값보다 쌀 경우
+			 *  minCost를 갱신해준다. 
+			 */
+			if(cost[i] < minCost) {
+				minCost = cost[i];
+			}
+			
+			sum += (minCost * dist[i]);	// 총 비용 누적 합
+		}
+		
+		System.out.println(sum);
+		
+	}
 }
