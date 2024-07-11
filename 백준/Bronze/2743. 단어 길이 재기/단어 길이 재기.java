bf143eb459ad79f823114eabc864bd50d2c0edfc
MINTAE KIM <112068306+minsoon025@users.noreply.github.com>
2024-07-11 11:41:31 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 92 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+    }
+}
